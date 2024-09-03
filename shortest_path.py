e606e227f22a71e30aec08e2547131c6520fa662
Özgür Kumaş <41588120+ozgurkumas@users.noreply.github.com>
2024-09-03 14:58:30 +0300

    Update shortest_path.py

--- shortest_path.py
+++ shortest_path.py
@@ -8,3 +8,18 @@ import tkinter as tk
 file = open('map_list/list_g', 'rb')
 G = pickle.load(file)
 file.close()
+
+class Node:
+    def __init__(self, id_, x, y):
+        self.id = id_
+        self.coords = [x, y]
+        self.outgoings = []
+
+node_dict = {}
+for node_id in G.nodes:
+    x, y = G.nodes[node_id]['x'], G.nodes[node_id]['y']
+    node_dict[node_id] = Node(node_id, x, y)
+
+for u, v, _ in G.edges(data=True):
+    node_dict[u].outgoings.append(node_dict[v])
+
