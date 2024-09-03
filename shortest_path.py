6df9ae69c8efd831b980fae1a06516aacc1f72e0
Özgür Kumaş <41588120+ozgurkumas@users.noreply.github.com>
2024-09-03 14:59:03 +0300

    Update shortest_path.py

--- shortest_path.py
+++ shortest_path.py
@@ -23,3 +23,60 @@ for node_id in G.nodes:
 for u, v, _ in G.edges(data=True):
     node_dict[u].outgoings.append(node_dict[v])
 
+def calcCost(A: Node, B: Node):
+    dist = ((A.coords[0] - B.coords[0])**2 + (A.coords[1] - B.coords[1])**2)**0.5
+    return dist
+
+def find_shortest_path(startState, goalState):
+    class PriorityQueue:
+        def __init__(self):
+            self.queue = []
+
+        def enqueue(self, list_):
+            self.queue.append(list_)
+
+        def dequeue(self, index):
+            self.queue.pop(index)
+
+        def peek(self):
+            index = 0
+            i = len(self.queue) - 1
+            min_cost = self.queue[-1][1]
+            while i >= 0:
+                if self.queue[i][1] <= min_cost:
+                    min_cost = self.queue[i][1]
+                    index = i
+                i -= 1
+            return index
+
+    prioQueue = PriorityQueue()
+    visitedList = []
+    parent = {}
+
+    prioQueue.enqueue([startState, 0])
+    parent[startState] = None
+
+    while True:
+        index = prioQueue.peek()
+        currentState = prioQueue.queue[index][0]
+
+        if currentState == goalState:
+            break
+        else:
+            if currentState not in visitedList:
+                for outgoing in currentState.outgoings:
+                    if outgoing not in visitedList:
+                        prioQueue.enqueue([outgoing, calcCost(currentState, outgoing) + prioQueue.queue[index][1]])
+                        parent[outgoing] = currentState
+                visitedList.append(currentState)
+            prioQueue.dequeue(index)
+
+    listPathNodes = []
+    currentNode = goalState
+    while currentNode is not None:
+        listPathNodes.append(currentNode)
+        currentNode = parent[currentNode]
+
+    listPathNodes.reverse()
+    return [node.id for node in listPathNodes]
+
